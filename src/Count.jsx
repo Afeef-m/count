bb5013ce8eb7ae8de121cfdbb82763246996e91e
Afeef-m <afeefmaliyekkal14@gmail.com>
2025-06-19 16:58:02 +0530

    new

--- src/Count.jsx
+++ src/Count.jsx
@@ -5,7 +5,6 @@ function Count(){
     let textcolor=count %2===0 ?'even':'odd'
     return(
 <div className="main">
-    
 <h1 className={`text ${textcolor}`}>Count:{count}</h1>
 <button onClick={()=>setCount(0)}>Reset</button>
 <button onClick={()=>setCount(count+1)}>Increment</button>
