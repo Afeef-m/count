b964ad8f50b7ce92e67335a0d44f16dc08555642
Afeef-m <afeefmaliyekkal14@gmail.com>
2025-06-19 16:54:55 +0530

    counter

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Count from './Count'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <Count/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Count.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from "react";
+function Count(){
+    let [count, setCount]=useState(0)
+    let textcolor=count %2===0 ?'even':'odd'
+    return(
+<div className="main">
+    
+<h1 className={`text ${textcolor}`}>Count:{count}</h1>
+<button onClick={()=>setCount(0)}>Reset</button>
+<button onClick={()=>setCount(count+1)}>Increment</button>
+<button onClick={()=>setCount(count-1)}>Decrement</button>
+</div>
+    )
+}
+export default Count;
